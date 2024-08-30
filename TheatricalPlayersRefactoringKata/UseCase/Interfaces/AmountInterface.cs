cabddc61a5524e2425b58a66c964c04bb161116b
William Rodrigues da Silva <wrsilva008@gmail.com>
2024-08-30 00:19:45 -0300

    [teste/william-silva] - "Criação de interface para implementação de metodos de calculos"

---
+++ TheatricalPlayersRefactoringKata/UseCase/Interfaces/AmountInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheatricalPlayersRefactoringKata.Entities;
+
+namespace TheatricalPlayersRefactoringKata.UseCase.Interfaces
+{
+    public interface AmountInterface
+    {
+        decimal AmountComedy(int audience, decimal amount);
+        decimal AmountHistory(int audience, decimal amount);
+        decimal AmountTragedy(int audience, decimal amount);
+    }
+}
