4ce4fa67ec2df1c426ebb0af6fa47750a158d46b
William Rodrigues da Silva <wrsilva008@gmail.com>
2024-08-30 00:35:33 -0300

    [teste/william-silva] - "Refatorando regra de negocio criando divisão de funções"

--- TheatricalPlayersRefactoringKata/UseCase/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/UseCase/StatementPrinter.cs
@@ -9,50 +9,29 @@ public class StatementPrinter
 {
     public string Print(Invoice invoice, Dictionary<string, Play> plays)
     {
-        var totalAmount = 0;
+        decimal totalAmount = 0;
         var volumeCredits = 0;
         var result = string.Format("Statement for {0}\n", invoice.Customer);
         CultureInfo cultureInfo = new CultureInfo("en-US");
 
         foreach (var perf in invoice.Performances)
         {
+            var calculate = new CalculatePerformance();
+
             var play = plays[perf.PlayId];
             var lines = play.Lines;
             if (lines < 1000) lines = 1000;
             if (lines > 4000) lines = 4000;
-            var thisAmount = lines * 10;
-            switch (play.Type)
-            {
-                case "tragedy":
-                    if (perf.Audience > 30)
-                    {
-                        thisAmount += 1000 * (perf.Audience - 30);
-                    }
-                    break;
-                case "comedy":
-                    if (perf.Audience > 20)
-                    {
-                        thisAmount += 10000 + 500 * (perf.Audience - 20);
-                    }
-                    thisAmount += 300 * perf.Audience;
-                    break;
-                case "history":
-                    if (perf.Audience > 20)
-                    {
-                        thisAmount += 11000 + 500 * (perf.Audience - 20);
-                    }
-                    thisAmount += 1800 * perf.Audience;
-                    break;
-                default:
-                    throw new Exception("unknown type: " + play.Type);
-            }
+            decimal thisAmount = lines / 10;
+            
+            thisAmount = calculate.CalculateAmount(perf.Audience, play, thisAmount);
             volumeCredits = CreateVolumeCredit(volumeCredits, perf, play);
 
             // print line for this order
-            result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDouble(thisAmount / 100), perf.Audience);
+            result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDouble(thisAmount), perf.Audience);
             totalAmount += thisAmount;
         }
-        result += string.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
+        result += string.Format(cultureInfo, "Amount owed is {0:C}\n", totalAmount);
         result += string.Format("You earned {0} credits\n", volumeCredits);
         return result;
     }
