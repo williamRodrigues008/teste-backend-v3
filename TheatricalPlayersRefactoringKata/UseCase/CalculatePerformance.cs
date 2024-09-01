49712a8c55edafc154df2e72a2ce9710d52103e8
William Rodrigues da Silva <wrsilva008@gmail.com>
2024-09-01 10:20:42 -0300

    [teste/william-silva] - "Refatorando metodo de calculo de performance e ajuste parametrização"

--- TheatricalPlayersRefactoringKata/UseCase/CalculatePerformance.cs
+++ TheatricalPlayersRefactoringKata/UseCase/CalculatePerformance.cs
@@ -10,39 +10,37 @@ namespace TheatricalPlayersRefactoringKata.UseCase
 {
     public class CalculatePerformance : AmountInterface
     {
-        public decimal CalculateAmount(int audience, Play play, decimal amount)
+        public decimal CalculateAmount(int audience, Play play)
         {
             return play.Type switch
             {
-                "comedy" => AmountComedy(audience, amount),
-                "history" => AmountHistory(audience, amount),
-                "tragedy" => AmountTragedy(audience, amount),
+                "comedy" => AmountComedy(audience, play.Lines),
+                "history" => AmountHistory(audience, play.Lines),
+                "tragedy" => AmountTragedy(audience, play.Lines),
                 _ => throw new Exception("unknown type: " + play.Type)
             };
         }
 
-        public decimal AmountComedy(int audience, decimal amount)
+        public decimal AmountComedy(int audience, int lines)
         {
-            if (audience > 20) amount += 100 + 5 * (audience - 20);
+            decimal amount = 0;
+            if (audience > 20) amount = 100 + 5 * (audience - 20);
             amount += 3 * audience; 
-            return amount;
+            return amount + lines / 10;
         }
 
-        public decimal AmountTragedy(int audience, decimal amount)
+        public decimal AmountTragedy(int audience, int lines)
         {
+            decimal amount = 0;
+
             if (audience > 30) amount += 10 * (audience - 30);
 
-            return amount;
+            return amount + lines / 10;
         }
 
-        public decimal AmountHistory(int audience, decimal amount)
+        public decimal AmountHistory(int audience, int lines)
         {
-            var tragedy = AmountTragedy(audience, amount);
-            var comedy = AmountComedy(audience, amount);
-            var history = tragedy + comedy;
-            return history;
-
-
+            return AmountTragedy(audience, lines) + AmountComedy(audience, lines);
         }
     }
 }
