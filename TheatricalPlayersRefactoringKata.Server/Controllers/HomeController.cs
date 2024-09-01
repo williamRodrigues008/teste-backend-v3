e7ac586183e221c75b938614beb5de77cb980f74
William Rodrigues da Silva <wrsilva008@gmail.com>
2024-09-01 12:07:32 -0300

    [teste/william-silva] - "criação de api para gerenciamento de dados da aplicação"

---
+++ TheatricalPlayersRefactoringKata.Server/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheatricalPlayersRefactoringKata.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("get")]
+        public IActionResult Get()
+        {
+            return View();
+        }
+
+    }
+}
