a37f8e3749e7d8c81425672cd66779d674bfc89e
William Rodrigues da Silva <wrsilva008@gmail.com>
2024-09-02 16:50:52 -0300

    [teste/william-silva] - "Criação de contexto de banco de dados"

---
+++ TheatricalPlayersRefactoringKata.Server/ContextDb/ContextDtaBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheatricalPlayersRefactoringKata.Server.Entities;
+
+namespace TheatricalPlayersRefactoringKata.Server.ContextDb
+{
+    public class ContextDtaBase : DbContext
+    {
+        public ContextDtaBase(DbContextOptions<ContextDtaBase> options) : base(options) { }
+
+        public DbSet<Play> Play { get; set; }
+    }
+}
