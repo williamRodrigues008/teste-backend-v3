14f63c068d1cdda353f65109fe969f76c46047fb
William Rodrigues da Silva <wrsilva008@gmail.com>
2024-09-02 16:55:36 -0300

    [teste/william-silva] - "criação de interface"

---
+++ TheatricalPlayersRefactoringKata.Server/Interfaces/PlayInterface.cs
@@ -0,0 +1,13 @@
+﻿using TheatricalPlayersRefactoringKata.Server.Entities;
+
+namespace TheatricalPlayersRefactoringKata.Server.Interfaces
+{
+    public interface PlayInterface
+    {
+        Task<IEnumerable<Play>> GetAllPlay();
+        void DeletePlay(Play play);
+        void UpdatePlay(Play play);
+        void NewPlay(Play play);
+        Task<bool> SavePlay();
+    }
+}
