569683f4df497705043c13b507f62d3aa5db54da
William Rodrigues da Silva <wrsilva008@gmail.com>
2024-09-02 16:59:54 -0300

    [teste/william-silva] - "criação de teste de arquivo xml"

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using ApprovalTests;
 using ApprovalTests.Reporters;
 using TheatricalPlayersRefactoringKata.Entities;
@@ -14,6 +15,7 @@ public class StatementPrinterTests
     [UseReporter(typeof(DiffReporter))]
     public void TestStatementExampleLegacy()
     {
+        var typeStatement = "txt";
         var plays = new Dictionary<string, Play>();
         plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
         plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
@@ -30,15 +32,46 @@ public class StatementPrinterTests
         );
 
         StatementPrinter statementPrinter = new StatementPrinter();
-        var result = statementPrinter.Print(invoice, plays);
+        var result = statementPrinter.Print(invoice, plays, typeStatement, Directory.GetCurrentDirectory());
 
         Approvals.Verify(result);
     }
 
     [Fact]
     [UseReporter(typeof(DiffReporter))]
+    public void TestXMLStatementExample()
+    {
+        var plays = new Dictionary<string, Play>();
+        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
+        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
+        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
+        plays.Add("henry-v", new Play("Henry V", 3227, "history"));
+        plays.Add("john", new Play("King John", 2648, "history"));
+        plays.Add("richard-iii", new Play("Richard III", 3718, "history"));
+
+        Invoice invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40),
+                new Performance("henry-v", 20),
+                new Performance("john", 39),
+                new Performance("henry-v", 20)
+            }
+        );
+        StatementPrinter statementPrinter = new StatementPrinter();
+        var result = statementPrinter.GenerateStatementXML(Directory.GetCurrentDirectory(), invoice, plays);
+
+        Approvals.VerifyXml(result);
+    }
+
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
     public void TestTextStatementExample()
     {
+        var typeStatement = "xml";
         var plays = new Dictionary<string, Play>();
         plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
         plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
@@ -61,7 +94,7 @@ public class StatementPrinterTests
         );
 
         StatementPrinter statementPrinter = new StatementPrinter();
-        var result = statementPrinter.Print(invoice, plays);
+        var result = statementPrinter.Print(invoice, plays, typeStatement, Directory.GetCurrentDirectory());
 
         Approvals.Verify(result);
     }
